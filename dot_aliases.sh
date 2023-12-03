3ccabab92ea295d2de2ae9be844c727e47801fb9
Luca <45152996+lbrame@users.noreply.github.com>
2023-12-03 14:02:33 +0100

    Fix aliases and zsh

--- dot_aliases.sh
+++ dot_aliases.sh
@@ -79,7 +79,7 @@ if [[ $(grep -i archlinux /etc/os-release) ]]; then
 
 # Media
 alias mp3dl='yt-dlp --extract-audio --audio-format mp3'
-alias oggdl='yt-dlp -extract-audio --audio-format ogg --audio-quality 5 -i'
+    alias audiodl='yt-dlp -x -f bestaudio'
 
 # Shortcuts
 alias edit_zsh_conf="$EDITOR ~.zhsrc"
